f9ac4d5659d5b8c86be8f36d521ad34043960ce0
root <iyaron8@gmail.com>
2017-05-23 11:34:11 -0700

    first commit

---
+++ vfwInfrastructure/CTypes.h
@@ -0,0 +1,26 @@
+/*
+ * CTypes.h
+ *
+ *  Created on: May 23, 2017
+ *      Author: root
+ */
+
+#ifndef CTYPES_H_
+#define CTYPES_H_
+
+#include <inttypes.h>
+
+typedef char			BS8;
+typedef unsigned char	BU8;
+
+typedef int16_t			BS16;
+typedef uint16_t		BU16;
+
+typedef int32_t			BS32;
+typedef uint32_t		BU32;
+
+typedef int64_t			BS64;
+typedef int64_t			BS64;
+typedef uint64_t		BU64;
+
+#endif /* CTYPES_H_ */
